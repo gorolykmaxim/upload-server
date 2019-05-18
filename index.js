5bff659f6a2a56fd242add933c5f0d092c38084c
gorolykmaxim <streetbattle52@gmail.com>
2019-05-18 13:11:56 +0300

    Move deployment logic into a separate deploy.js module
    Move deployment logic into a separate deploy.js module
    and cover it with unit-tests.
    Merge file.js and deploy.js.

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ var https = require('https');
 var express = require('express');
 var multer = require('multer');
 var mkdirp = Promise.promisify(require('mkdirp'));
-var file = require('./file');
-var html = require('./tpl');
-var pkg = require('./package.json');
 var serveIndex = require('serve-index');
 var argv = require('minimist')(process.argv.slice(2));
 var rimraf = Promise.promisify(require('rimraf'));
+var Deploy = require('./deploy');
+var html = require('./tpl');
+var pkg = require('./package.json');
+
 var app = express();
 
 var default_host = ip.address();
@@ -65,82 +66,9 @@ if(!fs.existsSync(default_folder)) {
 
 console.log('[' + new Date().toISOString() + '] - Serving files from folder:', default_folder);
 
-var pathResolver = new file.PathResolver(Promise, path, mkdirp);
-
-var storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, default_folder);
-  },
-  filename: function(req, file, cb) {
-    pathResolver.resolveRelativePath(default_folder, req.body.file).then(function (value) {
-      cb(null, value);
-    }).catch(function (err) {
-      cb(err, null)
-    });
-  }
-});
-
-var upload = multer({ storage: storage });
-
-app.use('/' + default_folder, serveIndex(process.cwd() + '/' + default_folder));
-app.use('/' + default_folder, express.static(process.cwd() + '/' + default_folder));
-
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
-app.get('/', function(req, res) {
-  res.send(html.template);
-});
-
-app.post('/', upload.any(), function(req, res) {
-  console.log('[' + new Date().toISOString() + '] - File uploaded:', req.files[0].path);
-  res.end();
-});
-
-app.post('/upload', upload.any(), function(req, res) {
-  console.log('[' + new Date().toISOString() + '] - File uploaded:', req.files[0].path);
-  res.redirect('/' + default_folder);
-  res.end();
-});
-
-app.post('/move', function (req, res) {
-  var newPathPromise = pathResolver.resolveRelativePath(default_folder, req.query.file);
-  var oldPathPromise = pathResolver.resolveRelativePath(default_folder, req.query.old_file, false);
-  Promise.all([oldPathPromise, newPathPromise]).then(function (resolvedPaths) {
-    var oldPath = resolvedPaths[0];
-    var newPath = resolvedPaths[1];
-    var oldResolvedPath = path.join(default_folder, oldPath);
-    var resolvedPath = path.join(default_folder, newPath);
-    return fs.renameAsync(oldResolvedPath, resolvedPath);
-  }).then(function () {
-    res.end();
-    console.log('[' + new Date().toISOString() + '] - ' + 'File moved from ' + req.query.old_file + ' to ' + req.query.file);
-  }).catch(function (err) {
-    res.status(500).end(err.toString() + '\n');
-  });
-});
-
-app.post('/delete', function (req, res) {
-  var absolutePath = null;
-  pathResolver.resolveRelativePath(default_folder, req.query.file, false).then(function (value) {
-    absolutePath = path.join(default_folder, value);
-    return fs.lstatAsync(absolutePath);
-  }).then(function (value) {
-    if (value.isDirectory()) {
-      return rimraf(absolutePath);
-    } else {
-      return fs.unlinkAsync(absolutePath);
-    }
-  }).then(function () {
-    res.end();
-    console.log('[' + new Date().toISOString() + '] - ' + 'File removed: ' + req.query.file);
-  }).catch(function (err) {
-    res.status(500).end(err.toString() + '\n');
-  });
-});
+var deploy = new Deploy(default_folder, Promise, multer, serveIndex, express, html, path, fs, mkdirp, rimraf, process,
+    console);
+deploy.serveOn(app);
 
 if(tls_enabled && cert_file && key_file) {
   var options = { key: fs.readFileSync(key_file), cert: fs.readFileSync(cert_file) };
