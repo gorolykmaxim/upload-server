4778a42ab8b6e983ccedd410df7c4b9da9519944
gorolykmaxim <streetbattle52@gmail.com>
2019-05-25 10:34:43 +0300

    Move front-end js into a reusable common.js

---
+++ views/assets/common.js
@@ -0,0 +1,33 @@
+function LogDisplayer(logPath, $logContainer) {
+    this.logPath = logPath;
+    this.$logContainer = $logContainer;
+    this.$errorContainer = null;
+}
+
+LogDisplayer.prototype.setErrorContainer = function($errorContainer) {
+    this.$errorContainer = $errorContainer;
+};
+
+LogDisplayer.prototype.displayFull = function() {
+    this.display(true);
+};
+
+LogDisplayer.prototype.display = function (isFull) {
+    var self = this;
+    isFull = isFull || false;
+    var webSocket = new WebSocket('ws://localhost:8090/');
+    webSocket.addEventListener('open', function (ev) {
+        webSocket.send(JSON.stringify({type: 'watch', file: self.logPath, fromStart: isFull}));
+    });
+    webSocket.addEventListener('message', function (ev) {
+        var change = JSON.parse(ev.data);
+        if (change.type === 'change' && change.file === self.logPath) {
+            change.changes.forEach(function (value) {
+                self.$logContainer.prepend('<p>' + value + '</p>');
+            });
+        } else if (change.type === 'error' && self.$errorContainer !== null) {
+            self.$errorContainer.removeClass('d-none');
+            self.$errorContainer.html(change.message);
+        }
+    });
+};
