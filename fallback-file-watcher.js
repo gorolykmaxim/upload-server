610dcfa749948246bd03a0647152dc94afb25c70
gorolykmaxim <streetbattle52@gmail.com>
2019-07-02 22:29:07 +0300

    Fix line interrupts in FallbackFileWatcher
    "tail" binary may emit incomplete lines into it's STDOUT and STDERR.
    We should not emit such incomplete lines though. Each time we
    receive an incomplete line from "tail" - we wait for a next chunk
    of data and append that incomplete line to it.
    This way interrupts in "tail" output will not serve as "line breaks"
    in the actual FallbackFileWatcher notifications.

--- fallback-file-watcher.js
+++ fallback-file-watcher.js
@@ -1,5 +1,23 @@
 const EventEmitter = require('events');
 
+class Buffer {
+    constructor() {
+        this.data = null;
+    }
+    write(data) {
+        this.data = data;
+    }
+    read() {
+        return this.data;
+    }
+    isEmpty() {
+        return this.data === null;
+    }
+    clear() {
+        this.data = null;
+    }
+}
+
 class FallbackFileWatcher extends EventEmitter {
     constructor(filename, binaryPath, childProcess, os) {
         super();
@@ -8,14 +26,31 @@ class FallbackFileWatcher extends EventEmitter {
         this.childProcess = childProcess;
         this.os = os;
         this.process = null;
+        this.stdoutBuffer = new Buffer();
+        this.stderrBuffer = new Buffer();
     }
     watch() {
         this.process = this.childProcess.spawn(this.binary, ['-f', this.filename]);
-        this.process.stdout.on('data', this._handleData.bind(this));
-        this.process.stderr.on('data', this._handleData.bind(this));
+        this.process.stdout.on('data', data => this._handleData(data, this.stdoutBuffer));
+        this.process.stderr.on('data', data => this._handleData(data, this.stderrBuffer));
     }
-    _handleData(data) {
-        data.toString().split(this.os.EOL).forEach(line => this.emit('line', line));
+    _handleData(data, buffer) {
+        data = data.toString();
+        if (!buffer.isEmpty()) {
+            // Non-empty buffer means, that last chunk of logs, received by the file watcher, had last line
+            // interrupted. This means that the first line of current chunk is the continuation of that line.
+            data = buffer.read() + data;
+            buffer.clear();
+        }
+        const lines = data.split(this.os.EOL);
+        const lastLine = lines.pop();
+        if (lastLine !== '') {
+            // We've just split a string by line-ending delimiter. That string didn't end with a line-ending symbol,
+            // it means that line was not complete. We will not emit that line and save it until next time, since next
+            // time we will receive the rest of that line.
+            buffer.write(lastLine);
+        }
+        lines.forEach(line => this.emit('line', line));
     }
     close() {
         this.process.kill('SIGKILL');
--- test/fallback-file-watcher.test.js
+++ test/fallback-file-watcher.test.js
@@ -13,7 +13,7 @@ describe('FallbackFileWatcher', () => {
     const filename = 'C:\\Users\\user\\myfile.txt';
     const binary = 'tail.exe';
     const onelineMessage  = 'One message';
-    const twolineMessage = 'Two lines\r\nOf messages';
+    const twolineMessage = 'Two lines' + os.EOL + 'Of messages';
     let watcher = null;
     let childProcess = null;
     let process = null;
@@ -36,7 +36,7 @@ describe('FallbackFileWatcher', () => {
             expect(line).to.be.equal(onelineMessage);
             done();
         });
-        writeTo(process.stdout, onelineMessage);
+        writeTo(process.stdout, onelineMessage + os.EOL);
     });
     it('should notify listeners about a multi-line stdout change', done => {
         watcher.watch();
@@ -49,7 +49,7 @@ describe('FallbackFileWatcher', () => {
                 done();
             }
         });
-        writeTo(process.stdout, twolineMessage);
+        writeTo(process.stdout, twolineMessage + os.EOL);
     });
     it('should notify listeners about a single-line stderr change', done => {
         watcher.watch();
@@ -57,7 +57,7 @@ describe('FallbackFileWatcher', () => {
             expect(line).to.be.equal(onelineMessage);
             done();
         });
-        writeTo(process.stderr, onelineMessage);
+        writeTo(process.stderr, onelineMessage + os.EOL);
     });
     it('should notify listeners about a multi-line stderr change', done => {
         watcher.watch();
@@ -70,12 +70,28 @@ describe('FallbackFileWatcher', () => {
                 done();
             }
         });
-        writeTo(process.stderr, twolineMessage);
+        writeTo(process.stderr, twolineMessage + os.EOL);
     });
     it('should terminate binary process on close', () => {
         watcher.watch();
         watcher.close();
         expect(process.kill).to.have.been.calledOnceWith();
+    });
+    it('should notify listeners about multiple multi-line changes, with some of the lines being interrupted', done => {
+        watcher.watch();
+        const linesGot = [];
+        const linesExpected = ['line1', 'line2', 'interrupted line', 'line 3', 'line 4', 'another interrupted line', 'line 5'];
+        watcher.on('line', line => {
+            linesGot.push(line);
+            if (linesExpected.length === linesGot.length) {
+                expect(linesGot).to.eql(linesExpected);
+                done();
+            }
+        });
+        writeTo(process.stdout, [linesExpected[0], linesExpected[1], linesExpected[2].split(' ')[0]].join(os.EOL));
+        writeTo(process.stdout, [' ' + linesExpected[2].split(' ')[1], linesExpected[5].split(' ')[0] + ' '].join(os.EOL));
+        writeTo(process.stderr, [linesExpected[3], linesExpected[4]].join(os.EOL) + os.EOL);
+        writeTo(process.stdout, [linesExpected[5].split(' ')[1] + ' ' + linesExpected[5].split(' ')[2], linesExpected[6]].join(os.EOL) + os.EOL);
     });
 });
 
