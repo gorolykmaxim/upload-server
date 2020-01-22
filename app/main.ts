d4bcab3acf0e8ce0394b7f7be7d574447064fd6e
gorolykmaxim <streetbattle52@gmail.com>
2020-01-22 15:22:54 +0200

    Move upload-server initialization into the main() function

--- app/main.ts
+++ app/main.ts
@@ -9,7 +9,7 @@ import {ValuesCollection} from "./common/collection/values-collection";
 import {LogFile} from "./log-watcher/log/log-file";
 import {LogFileCollection} from "./log-watcher/log/log-file-collection";
 import {LegacyWebSocketAPI} from "./log-watcher/api/legacy-web-socket-api";
-import {CreateUUID, WatcherFactory} from "./log-watcher/watcher/watcher-factory";
+import {WatcherFactory} from "./log-watcher/watcher/watcher-factory";
 import {MessageFactory} from "./log-watcher/watcher/message-factory";
 import {LegacyMessageFactory} from "./log-watcher/watcher/legacy-message-factory";
 import {DefaultMessageFactory} from "./log-watcher/watcher/default-message-factory";
@@ -18,37 +18,42 @@ import {URL} from "./common/url";
 import {RestAPI} from "./log-watcher/api/rest-api";
 import * as expressWs from "express-ws";
 import {WebSocketAPI} from "./log-watcher/api/web-socket-api";
+import {CreateUUID} from "./common/uuid";
 import bodyParser = require("body-parser");
 import Bluebird = require("bluebird");
 import uuid = require("uuid");
 
-const Tail = require('nodejs-tail');
-const fileSystem = Bluebird.promisifyAll(require('fs'));
+async function main(): Promise<void> {
+    const Tail = require('nodejs-tail');
+    const fileSystem = Bluebird.promisifyAll(require('fs'));
 
-const createTail: CreateTail = absolutePathToFile => new Tail(absolutePathToFile, {usePolling: true});
-const logFileFactory: LogFileFactory = new UnixLogFileFactory(createTail, fileSystem, EOL);
-const allowedLogs: Collection<string> = new ValuesCollection();
-const logFiles: Collection<LogFile> = new LogFileCollection();
-const logFilePool: LogFilePool = new RestrictedLogFilePool(allowedLogs, logFiles, logFileFactory);
+    const createTail: CreateTail = absolutePathToFile => new Tail(absolutePathToFile, {usePolling: true});
+    const logFileFactory: LogFileFactory = new UnixLogFileFactory(createTail, fileSystem, EOL);
+    const allowedLogs: Collection<string> = new ValuesCollection();
+    const logFiles: Collection<LogFile> = new LogFileCollection();
+    const logFilePool: LogFilePool = new RestrictedLogFilePool(allowedLogs, logFiles, logFileFactory);
 
-const application: any = express();
-expressWs(application);
-application.use(bodyParser.json());
-const createUUID: CreateUUID = () => uuid();
+    const application: any = express();
+    expressWs(application);
+    application.use(bodyParser.json());
+    const createUUID: CreateUUID = () => uuid();
 
-const legacyMessageFactory: MessageFactory = new LegacyMessageFactory();
-const legacyWatcherFactory: WatcherFactory = new WatcherFactory(createUUID, legacyMessageFactory);
-const legacyAPI: WebSocketAPI = new LegacyWebSocketAPI(logFilePool, legacyWatcherFactory);
-application.ws('/', legacyAPI.onConnectionOpen.bind(legacyAPI));
+    const legacyMessageFactory: MessageFactory = new LegacyMessageFactory();
+    const legacyWatcherFactory: WatcherFactory = new WatcherFactory(createUUID, legacyMessageFactory);
+    const legacyAPI: WebSocketAPI = new LegacyWebSocketAPI(logFilePool, legacyWatcherFactory);
+    application.ws('/', legacyAPI.onConnectionOpen.bind(legacyAPI));
 
-const baseAPIURL: URL = URL.createNew('api');
-const baseLogWatcherAPIURL: URL = baseAPIURL.append('log-watcher');
+    const baseAPIURL: URL = URL.createNew('api');
+    const baseLogWatcherAPIURL: URL = baseAPIURL.append('log-watcher');
 
-const messageFactory: MessageFactory = new DefaultMessageFactory();
-const watcherFactory: WatcherFactory = new WatcherFactory(createUUID, messageFactory);
-const defaultWebSocketAPI: WebSocketAPI = new DefaultWebSocketAPI(logFilePool, watcherFactory);
-application.ws(baseLogWatcherAPIURL.append('log').value, defaultWebSocketAPI.onConnectionOpen.bind(defaultWebSocketAPI));
+    const messageFactory: MessageFactory = new DefaultMessageFactory();
+    const watcherFactory: WatcherFactory = new WatcherFactory(createUUID, messageFactory);
+    const defaultWebSocketAPI: WebSocketAPI = new DefaultWebSocketAPI(logFilePool, watcherFactory);
+    application.ws(baseLogWatcherAPIURL.append('log').value, defaultWebSocketAPI.onConnectionOpen.bind(defaultWebSocketAPI));
 
-const restAPI: RestAPI = new RestAPI(baseLogWatcherAPIURL, application, allowedLogs, logFilePool, fileSystem);
+    const restAPI: RestAPI = new RestAPI(baseLogWatcherAPIURL, application, allowedLogs, logFilePool, fileSystem);
 
-application.listen(8080, () => {console.log('Listening on 8080...')});
+    application.listen(8080, () => {console.log('Listening on 8080...')});
+}
+
+main();
