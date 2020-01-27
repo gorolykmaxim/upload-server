75dc4aeda6e0af1295cabb0164558a22286b7ba7
gorolykmaxim <streetbattle52@gmail.com>
2020-01-27 17:27:35 +0200

    Reduce code duplication in command API tests

--- test/command-executor/api/rest/command/create-command.spec.ts
+++ test/command-executor/api/rest/command/create-command.spec.ts
@@ -1,44 +1,21 @@
-import {Command} from "../../../../../app/command-executor/command/command";
-import {Collection} from "../../../../../app/common/collection/collection";
-import {Request, Response} from "express";
 import {APIRequest} from "../../../../../app/common/api/request";
-import {CommandFactory} from "../../../../../app/command-executor/command/command-factory";
-import {instance, mock, verify, when} from "ts-mockito";
+import {instance, verify} from "ts-mockito";
 import {CreateCommand} from "../../../../../app/command-executor/api/rest/command/create-command";
 import {ExecutableCommand} from "../../../../../app/command-executor/api/rest/command/executable-command";
+import {Mocks} from "./mocks";
 
 describe('CreateCommand', function () {
-    const args = {name: 'list all files', script: 'ls -lh'};
-    let commandMock: Command;
-    let command: Command;
-    let factory: CommandFactory;
-    let commands: Collection<Command>;
-    let reqMock: Request;
-    let resMock: Response;
-    let req: Request;
-    let res: Response;
+    let mocks: Mocks;
     let request: APIRequest;
     beforeEach(function () {
-        factory = mock(CommandFactory);
-        commandMock = mock(Command);
-        command = instance(commandMock);
-        commands = mock<Collection<Command>>();
-        reqMock = mock<Request>();
-        resMock = mock<Response>();
-        req = instance(reqMock);
-        res = instance(resMock);
-        when(commandMock.id).thenReturn('12345');
-        when(commandMock.name).thenReturn(args.name);
-        when(commandMock.script).thenReturn(args.script);
-        when(factory.create(args.name, args.script)).thenReturn(command);
-        when(reqMock.body).thenReturn(args);
-        request = CreateCommand.create(instance(factory), instance(commands));
+        mocks = new Mocks();
+        request = CreateCommand.create(instance(mocks.factory), instance(mocks.commands));
     });
     it('should create specified command and return information about it', async function () {
         // when
-        await request.process(req, res);
+        await request.process(mocks.req, mocks.res);
         // then
-        verify(commands.add(command)).once();
-        verify(resMock.end(JSON.stringify(new ExecutableCommand(command)))).once();
+        verify(mocks.commands.add(mocks.command)).once();
+        verify(mocks.resMock.end(JSON.stringify(new ExecutableCommand(mocks.command)))).once();
     });
 });
--- test/command-executor/api/rest/command/find-all-commands.spec.ts
+++ test/command-executor/api/rest/command/find-all-commands.spec.ts
@@ -1,38 +1,20 @@
-import {Request, Response} from "express";
 import {FindAllCommands} from "../../../../../app/command-executor/api/rest/command/find-all-commands";
-import {Collection} from "../../../../../app/common/collection/collection";
-import {Command} from "../../../../../app/command-executor/command/command";
-import {instance, mock, verify, when} from "ts-mockito";
+import {instance, verify} from "ts-mockito";
 import {APIRequest} from "../../../../../app/common/api/request";
 import {ExecutableCommand} from "../../../../../app/command-executor/api/rest/command/executable-command";
+import {Mocks} from "./mocks";
 
 describe('FindAllCommands', function () {
-    let commandMock: Command;
-    let command: Command;
-    let commands: Collection<Command>;
-    let reqMock: Request;
-    let resMock: Response;
-    let req: Request;
-    let res: Response;
+    let mocks: Mocks;
     let request: APIRequest;
     beforeEach(function () {
-        commandMock = mock(Command);
-        command = instance(commandMock);
-        commands = mock<Collection<Command>>();
-        reqMock = mock<Request>();
-        resMock = mock<Response>();
-        req = instance(reqMock);
-        res = instance(resMock);
-        when(commandMock.id).thenReturn('12345');
-        when(commandMock.name).thenReturn('list all files');
-        when(commandMock.script).thenReturn('ls -lh');
-        when(commands.findAll()).thenResolve([command]);
-        request = FindAllCommands.create(instance(commands));
+        mocks = new Mocks();
+        request = FindAllCommands.create(instance(mocks.commands));
     });
     it('should find all commands, that can be executed', async function () {
         // when
-        await request.process(req, res);
+        await request.process(mocks.req, mocks.res);
         // then
-        verify(resMock.end(JSON.stringify([new ExecutableCommand(command)]))).once();
+        verify(mocks.resMock.end(JSON.stringify([new ExecutableCommand(mocks.command)]))).once();
     });
 });
