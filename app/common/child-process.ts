bc1b66b2b305db1b52a21f1a09eb1f7749f3fb2a
gorolykmaxim <streetbattle52@gmail.com>
2020-02-02 13:48:40 +0200

    Get rid of current implementation

--- app/common/child-process.ts
+++
@@ -1,6 +0,0 @@
-import {ChildProcess} from "child_process";
-
-/**
- * Factory-method, that creates a child process of the specified binary, while passing specified args to it.
- */
-export type CreateChildProcess = (binaryPath: string, args: Array<string>) => ChildProcess;
--- app/common/uuid.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Factory-method that returns a new UUID in as a string.
- */
-export type CreateUUID = () => string;
