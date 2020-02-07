eb04228922bf1ed25796fa10cbb31c34a88c61f5
gorolykmaxim <streetbattle52@gmail.com>
2020-02-07 23:02:03 +0200

    Implement read-file.ts

---
+++ backend/core/file-system/read-file.ts
@@ -0,0 +1,21 @@
+import {FileSystemCommand} from "./base";
+import {ArgumentsConsumer} from "../command/command-with-arguments";
+import {Observable, Subscriber} from "rxjs";
+
+export const READ_FILE: string = 'read file';
+
+/**
+ * Read contents of the file, located by the specified path.
+ */
+export class ReadFile extends FileSystemCommand implements ArgumentsConsumer {
+    readonly mandatoryArgs: Array<string> = ['path'];
+
+    /**
+     * {@inheritDoc}
+     */
+    async execute(output: Subscriber<any>, args?: any, input?: Observable<any>): Promise<void> {
+        const data: string | Buffer = await this.fileSystem.readFile(args.path, args);
+        output.next(data.toString());
+        output.complete();
+    }
+}
