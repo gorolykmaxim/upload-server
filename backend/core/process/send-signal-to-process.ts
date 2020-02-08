eaae9ce8563e9dba3f4c65df1a24e56cddbc584e
gorolykmaxim <streetbattle52@gmail.com>
2020-02-08 12:33:39 +0200

    Implement send-signal-to-process.ts

---
+++ backend/core/process/send-signal-to-process.ts
@@ -0,0 +1,28 @@
+import {Process, ProcessCommand, ProcessWithPIDIsNotRunning} from "./base";
+import {ArgumentsConsumer} from "../command/command-with-arguments";
+import {Observable, Subscriber} from "rxjs";
+
+export const SEND_SIGNAL_TO_PROCESS: string = 'send signal to process';
+
+/**
+ * Send a signal to the process with the specified pid.
+ * The command finishes immediately.
+ */
+export class SendSignalToProcess extends ProcessCommand implements ArgumentsConsumer {
+    readonly mandatoryArgs: Array<string> = ['pid', 'signal'];
+
+    /**
+     * {@inheritDoc}
+     */
+    async execute(output: Subscriber<any>, args?: any, input?: Observable<any>): Promise<void> {
+        const pid: number = args.pid;
+        const signal: number = args.signal;
+        const process: Process = this.pidToProcess.getValue(pid);
+        if (!process) {
+            output.error(new Error(`Failed to send signal ${signal}: ${new ProcessWithPIDIsNotRunning(pid).message}`));
+        } else {
+            process.childProcess.kill(signal);
+            output.complete();
+        }
+    }
+}
---
+++ test/core/process/send-signal-to-process.spec.ts
@@ -0,0 +1,32 @@
+import {Dictionary} from "typescript-collections";
+import {Process} from "../../../backend/core/process/base";
+import {ChildProcess} from "child_process";
+import {Command} from "../../../backend/core/command/command";
+import {instance, mock, verify} from "ts-mockito";
+import {SendSignalToProcess} from "../../../backend/core/process/send-signal-to-process";
+import {executeAndReturnOutput} from "../../common";
+import {constants} from "os";
+import { expect } from "chai";
+
+describe('SendSignalToProcess', function () {
+    const pid: number = 12345;
+    let pidToProcess: Dictionary<number, Process>;
+    let childProcess: ChildProcess;
+    let command: Command;
+    beforeEach(function () {
+        pidToProcess = new Dictionary<number, Process>();
+        childProcess = mock<ChildProcess>();
+        pidToProcess.setValue(pid, {childProcess: instance(childProcess)});
+        command = new SendSignalToProcess(pidToProcess);
+    });
+    it('should send the specified signal to the specified process', async function () {
+        // when
+        await executeAndReturnOutput(command, {pid: pid, signal: constants.signals.SIGINT}).toPromise();
+        // then
+        verify(childProcess.kill(constants.signals.SIGINT)).once();
+    });
+    it('should fail since the process with the specified PID does not exist', async function () {
+        // then
+        await expect(executeAndReturnOutput(command, {pid: 5426, signal: constants.signals.SIGKILL}).toPromise()).rejectedWith(Error);
+    });
+});
