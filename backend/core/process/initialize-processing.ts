4487f548e96ce83d1303ae9532d06bbc7a28366b
gorolykmaxim <streetbattle52@gmail.com>
2020-02-09 13:28:26 +0200

    Implement initialize-processing.ts

---
+++ backend/core/process/initialize-processing.ts
@@ -0,0 +1,39 @@
+import {Command} from "../command/command";
+import {Observable, Subscriber} from "rxjs";
+import {CREATE_PROCESS, CreateChildProcess, CreateProcess} from "./create-process";
+import {Dictionary} from "typescript-collections";
+import {CommandExecutor} from "../command/command-executor";
+import {CommandWithArguments} from "../command/command-with-arguments";
+import {SEND_SIGNAL_TO_PROCESS, SendSignalToProcess} from "./send-signal-to-process";
+import {WATCH_PROCESS_OUTPUT, WatchProcessOutput} from "./watch-process-output";
+import {Process} from "./base";
+
+export const INITIALIZE_PROCESSING: string = 'initialize processing';
+
+/**
+ * Initialize processing sub-system.
+ */
+export class InitializeProcessing extends Command {
+    /**
+     * Construct a command.
+     *
+     * @param commandExecutor command executor to register processing-related commands in
+     * @param createChildProcess factory method to create the actual process
+     * @param eol end-of-line separator of STDOUT and STDERR of processes
+     */
+    constructor(private commandExecutor: CommandExecutor, private createChildProcess: CreateChildProcess,
+                private eol: string) {
+        super();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    async execute(output: Subscriber<any>, args?: any, input?: Observable<any>): Promise<void> {
+        const pidToProcess: Dictionary<number, Process> = new Dictionary<number, Process>();
+        this.commandExecutor.register(CREATE_PROCESS, new CommandWithArguments(new CreateProcess(this.createChildProcess, pidToProcess)));
+        this.commandExecutor.register(SEND_SIGNAL_TO_PROCESS, new CommandWithArguments(new SendSignalToProcess(pidToProcess)));
+        this.commandExecutor.register(WATCH_PROCESS_OUTPUT, new CommandWithArguments(new WatchProcessOutput(pidToProcess, this.eol)));
+        output.complete();
+    }
+}
---
+++ test/core/process/initialize-processing.spec.ts
@@ -0,0 +1,26 @@
+import {CommandExecutor} from "../../../backend/core/command/command-executor";
+import {Command} from "../../../backend/core/command/command";
+import {anything, instance, mock, verify} from "ts-mockito";
+import {InitializeProcessing} from "../../../backend/core/process/initialize-processing";
+import {executeAndReturnOutput} from "../../common";
+import {CREATE_PROCESS} from "../../../backend/core/process/create-process";
+import {SEND_SIGNAL_TO_PROCESS} from "../../../backend/core/process/send-signal-to-process";
+import {WATCH_PROCESS_OUTPUT} from "../../../backend/core/process/watch-process-output";
+import {EOL} from "os";
+
+describe('InitializeProcessing', function () {
+    let executor: CommandExecutor;
+    let command: Command;
+    beforeEach(function () {
+        executor = mock(CommandExecutor);
+        command = new InitializeProcessing(instance(executor), null, EOL);
+    });
+    it('should register all processing-related commands', async function () {
+        // when
+        await executeAndReturnOutput(command).toPromise();
+        // then
+        verify(executor.register(CREATE_PROCESS, anything())).once();
+        verify(executor.register(SEND_SIGNAL_TO_PROCESS, anything())).once();
+        verify(executor.register(WATCH_PROCESS_OUTPUT, anything())).once();
+    });
+});
