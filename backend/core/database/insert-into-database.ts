c04682cd3014ce1750a36bee7858e0d63e6eddc6
gorolykmaxim <streetbattle52@gmail.com>
2020-02-09 18:36:41 +0200

    Implement insert-into-database.ts

---
+++ backend/core/database/insert-into-database.ts
@@ -0,0 +1,35 @@
+import {DatabaseCommand} from "./base";
+import {ArgumentsConsumer} from "../command/command-with-arguments";
+import {Observable, Subscriber} from "rxjs";
+import {toArray} from "rxjs/operators";
+
+export const INSERT_INTO_DATABASE: string = 'insert into database';
+
+/**
+ * Inserts each element from the input to the specified "table" in the database.
+ */
+export class InsertIntoDatabase extends DatabaseCommand implements ArgumentsConsumer {
+    readonly mandatoryArgs: Array<string> = ['table'];
+
+    /**
+     * {@inheritDoc}
+     */
+    async execute(output: Subscriber<any>, args?: any, input?: Observable<any>): Promise<void> {
+        const table: string = args.table;
+        const rows: Array<any> = await input.pipe(toArray()).toPromise();
+        for (let row of rows) {
+            const columns: Array<string> = [];
+            const placeholders: Array<string> = [];
+            const values: Array<any> = [];
+            for (let key in row) {
+                if (row.hasOwnProperty(key)) {
+                    columns.push(key);
+                    placeholders.push('?');
+                    values.push(row[key]);
+                }
+            }
+            await this.database.run(`INSERT INTO ${table}(${columns.join(', ')}) VALUES(${placeholders.join(', ')})`, ...values);
+        }
+        output.complete();
+    }
+}
---
+++ test/core/database/insert-into-database.spec.ts
@@ -0,0 +1,28 @@
+import {Command} from "../../../backend/core/command/command";
+import {Database} from "sqlite";
+import {instance, mock, verify} from "ts-mockito";
+import {InsertIntoDatabase} from "../../../backend/core/database/insert-into-database";
+import {executeAndReturnOutput} from "../../common";
+import {from} from "rxjs";
+
+describe('InsertIntoDatabase', function () {
+    const table: string = 'my_table';
+    let database: Database;
+    let command: Command;
+    beforeEach(function () {
+        database = mock<Database>();
+        command = new InsertIntoDatabase(instance(database));
+    });
+    it('should insert elements from the input into the database', async function () {
+        // given
+        const elementsToInsert: Array<any> = [
+            {'a': 15, 'b': 'c'},
+            {'a': 16, 'b': 'd'}
+        ];
+        // when
+        await executeAndReturnOutput(command, {table: table}, from(elementsToInsert)).toPromise();
+        // then
+        verify(database.run(`INSERT INTO ${table}(a, b) VALUES(?, ?)`, 15, 'c')).once();
+        verify(database.run(`INSERT INTO ${table}(a, b) VALUES(?, ?)`, 16, 'd')).once();
+    });
+});
