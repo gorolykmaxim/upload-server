66d1179c6c47a327575da2f933875013c6f17d76
gorolykmaxim <streetbattle52@gmail.com>
2020-02-18 21:13:36 +0200

    Fix empty first line in append-only-list.component.ts

--- frontend/src/app/widget/append-only-list/append-only-list.component.ts
+++ frontend/src/app/widget/append-only-list/append-only-list.component.ts
@@ -11,13 +11,15 @@ export class AppendOnlyListComponent implements AfterViewInit, OnDestroy {
   @Input() bufferIntervalMilliseconds = 500;
   @Input() autoScroll = false;
   @ViewChild('container') container: ElementRef;
-  serializedList = '';
+  serializedList: string = null;
   listSubscription: Subscription;
   autoScrollSubscription: Subscription;
 
   ngAfterViewInit(): void {
     const bufferedObservable: Observable<Array<string>> = this.listObservable.pipe(bufferTime(this.bufferIntervalMilliseconds));
-    this.listSubscription = bufferedObservable.subscribe(values => this.serializedList = [this.serializedList].concat(values).join('\n'));
+    this.listSubscription = bufferedObservable.subscribe(values => {
+      this.serializedList = this.serializedList !== null ? [this.serializedList].concat(values).join('\n') : values.join('\n');
+    });
     // DOM will not get re-rendered on this event loop iteration, so there is no reason to scroll to the bottom right away.
     // Scroll to the bottom with 1 ms delay, so that we will do it on the next iteration when the DOM will get updated.
     this.autoScrollSubscription = bufferedObservable.pipe(delay(1)).subscribe(_ => {
