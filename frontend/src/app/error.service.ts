af0c6e528ef5e76b1fd2b6f4742054ef3377ff95
gorolykmaxim <streetbattle52@gmail.com>
2020-02-20 08:49:31 +0200

    Implement initial simple version of error.service.ts
    The version that just displays error snackbar.

---
+++ frontend/src/app/error.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {ReplaySubject, Subject} from 'rxjs';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+@Injectable({providedIn: 'root'})
+export class ErrorService {
+  errorSubject: Subject<Error> = new ReplaySubject();
+
+  constructor(private snackBar: MatSnackBar) {
+  }
+
+  log(error: Error): void {
+    this.snackBar.open(error.message, null, {duration: 5000});
+    this.errorSubject.next(error);
+  }
+}
