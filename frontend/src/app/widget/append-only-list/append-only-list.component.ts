f03f24ec6916d986371c36027bb2ad8e880f10b3
gorolykmaxim <streetbattle52@gmail.com>
2020-02-20 11:59:27 +0200

    Improvements of append-only-list.component
    Add interval padding.
    Simplify lifecycle hooks handling.
    Fix scrolling issue: components scrolls down every buffering
    interval.
    Reinitialize subscriptions only when the listObservable is
    changed.
    Empty serializedList on new listObservable.
    If autoScroll values changes from false to true - immediately
    do one scroll down.

--- frontend/src/app/widget/append-only-list/append-only-list.component.ts
+++ frontend/src/app/widget/append-only-list/append-only-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, Input, OnChanges, OnDestroy, SimpleChanges, ViewChild} from '@angular/core';
 import {Observable, Subscription} from 'rxjs';
 import {bufferTime, delay} from 'rxjs/operators';
 
@@ -6,7 +6,7 @@ import {bufferTime, delay} from 'rxjs/operators';
   selector: 'app-append-only-list',
   templateUrl: './append-only-list.component.html'
 })
-export class AppendOnlyListComponent implements OnInit, OnDestroy {
+export class AppendOnlyListComponent implements OnChanges, OnDestroy {
   @Input() listObservable: Observable<string>;
   @Input() bufferIntervalMilliseconds = 500;
   @Input() autoScroll = false;
@@ -15,25 +15,38 @@ export class AppendOnlyListComponent implements OnInit, OnDestroy {
   listSubscription: Subscription;
   autoScrollSubscription: Subscription;
 
-  ngOnInit(): void {
-    const bufferedObservable: Observable<Array<string>> = this.listObservable.pipe(bufferTime(this.bufferIntervalMilliseconds));
-    this.listSubscription = bufferedObservable.subscribe(values => {
-      if (this.serializedList !== null) {
-        this.serializedList = [this.serializedList].concat(values).join('\n');
-      } else if (values.length > 0) {
-        this.serializedList = values.join('\n');
-      }
-    });
-    // DOM will not get re-rendered on this event loop iteration, so there is no reason to scroll to the bottom right away.
-    // Scroll to the bottom with 1 ms delay, so that we will do it on the next iteration when the DOM will get updated.
-    this.autoScrollSubscription = bufferedObservable.pipe(delay(1)).subscribe(_ => {
-      if (this.autoScroll) {
-        this.container.nativeElement.scroll({
-          top: this.container.nativeElement.scrollHeight,
-          left: 0,
-          behavior: 'smooth'
-        });
-      }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.listObservable?.currentValue) {
+      this.listSubscription?.unsubscribe();
+      this.autoScrollSubscription?.unsubscribe();
+      this.serializedList = null;
+      this.listObservable = changes.listObservable.currentValue;
+      const bufferedObservable: Observable<Array<string>> = this.listObservable.pipe(bufferTime(this.bufferIntervalMilliseconds));
+      this.listSubscription = bufferedObservable.subscribe(values => {
+        if (this.serializedList !== null) {
+          this.serializedList = [this.serializedList].concat(values).join('\n');
+        } else if (values.length > 0) {
+          this.serializedList = values.join('\n');
+        }
+      });
+      // DOM will not get re-rendered on this event loop iteration, so there is no reason to scroll to the bottom right away.
+      // Scroll to the bottom with 1 ms delay, so that we will do it on the next iteration when the DOM will get updated.
+      this.autoScrollSubscription = bufferedObservable.pipe(delay(1)).subscribe(values => {
+        if (values.length > 0 && this.autoScroll) {
+          this.scrollToBottom();
+        }
+      });
+    }
+    if (changes.autoScroll?.currentValue) {
+      this.scrollToBottom();
+    }
+  }
+
+  scrollToBottom(): void {
+    this.container?.nativeElement.scroll({
+      top: this.container.nativeElement.scrollHeight,
+      left: 0,
+      behavior: 'smooth'
     });
   }
 
