6797de0d8caa326be4af86735a1c69ca8849c36f
gorolykmaxim <streetbattle52@gmail.com>
2020-02-21 09:02:23 +0200

    Finalize application-log.component

--- frontend/src/app/application-log/application-log.component.ts
+++ frontend/src/app/application-log/application-log.component.ts
@@ -1,17 +1,22 @@
-import {Component} from '@angular/core';
-import {interval, Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {Component, OnInit} from '@angular/core';
+import {Log} from '../log-watcher.service';
+import {ApplicationLogService} from '../application-log.service';
+import {EMPTY} from 'rxjs';
 
 @Component({
   selector: 'app-application-log',
   templateUrl: './application-log.component.html',
 })
-export class ApplicationLogComponent {
-  generatedList: Observable<string> = interval(50).pipe(map(v => `Item #${v}`));
+export class ApplicationLogComponent implements OnInit {
+  log: Log = new Log(EMPTY, 0);
 
-  constructor() { }
+  constructor(private applicationLogService: ApplicationLogService) { }
 
-  testPrint(): void {
-    console.log('test print');
+  ngOnInit(): void {
+    this.watchLog(false);
+  }
+
+  async watchLog(full: boolean): Promise<void> {
+    this.log = await this.applicationLogService.getLog(full);
   }
 }
