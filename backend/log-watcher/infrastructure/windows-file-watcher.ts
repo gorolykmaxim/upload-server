b71c617d2370852c457298efa05fee1810077528
gorolykmaxim <streetbattle52@gmail.com>
2020-02-24 17:56:18 +0200

    Fix log watch on windows
    Make windows-file-watcher.ts behave the same as unix-file-watcher.ts:
    watch() should return observable only of yet-to-be-introduced changes.
    It should not return existing log lines. Otherwise, on windows the observable,
    that emits all the existing log lines + the new ones will emit last couple
    of existing lines multiple times.

--- backend/log-watcher/infrastructure/windows-file-watcher.ts
+++ backend/log-watcher/infrastructure/windows-file-watcher.ts
@@ -15,7 +15,7 @@ export class WindowsFileWatcher implements FileWatcher {
         let output: Observable<string> = this.pathToOutputObservable[path];
         if (!output) {
             output = new Observable<string>(subscriber => {
-                const tail: ChildProcess = spawn(this.pathToTailBinary, ['-f', path]);
+                const tail: ChildProcess = spawn(this.pathToTailBinary, ['-n', '0', '-f', path]);
                 fromEvent(createInterface(tail.stdout), 'line').subscribe(subscriber);
                 fromEvent(createInterface(tail.stderr), 'line').subscribe(subscriber);
                 subscriber.add(() => tail.kill(constants.signals.SIGINT));
